b326c2ad6c9a2fea13874f84b58229cb3efe6daa
Sleeper83091 <timothy.sleeper830@gmail.com>
2015-07-09 16:20:14 -0700

    making a new default rep with todays assignment

---
+++ root/js/elements2.js
@@ -0,0 +1,33 @@
+var movies = [
+	["The Lord of the Rings", "9 hours", "2001-2005"],
+	["Star Wars", "12+hours", "1977 - present"],
+	["The Bourne Identity", "1h59m", "2002"],
+	["There Will Be Blood", "2h38m", "2007"],
+	["Gangs of New York", "3h36m", "2002"]
+];
+
+var movieList = document.createElement("ul");
+movieList.setAttribute("id", "movieList");
+document.body.appendChild(movieList);
+
+
+for(var i =0; i < movies.length; i++){
+	var movie = movies[i];
+	movieObj = new Movie(movie[0], movie[1], movie[2]);
+	e("li", movieObj.title);
+}
+
+function Movie(name, time, year){
+		this.title = name;
+		this.time = time;
+		this.year = year;
+	}
+
+
+function e(elementType, text){
+	var newE = document.createElement(elementType);
+	newE.textContent = text;
+	movieList.appendChild(newE);
+	
+}
+
